10b59f9342d9bc79859d781cbfa5c96fb0b3f7d9
reflektoin <66482139+reflektoin@users.noreply.github.com>
2020-10-22 23:38:15 +0300

    Lisää sql-komennon taulun luomiseksi

--- csv-to-mysql-db.py
+++ csv-to-mysql-db.py
@@ -1,6 +1,8 @@
 import csv
 import MySQLdb
 #Alla oleva skripti on tehty "tutki hankintoja" -dataa (https://www.avoindata.fi/data/fi/dataset/tutkihankintoja-data) silmällä pitäen
+#komento hankinnat-taulun luontiin "create table hankinnat (lasku_id varchar(8), tili varchar(40) , tiliointisumma numeric(10,2), tositepvm varchar(10), toimittaja_y_tunnus varchar(10), toimittaja_nimi  varchar(50), hankintayksikko_tunnus varchar(5), hankintayksikko varchar(128), ylaorganisaatio_tunnus varchar(5), ylaorganisaatio varchar(128), sektori varchar(128), tuote_palveluryhma varchar(128), hankintakategoria varchar(128));
+"
 mydb = MySQLdb.connect(host='127.0.0.1',
                            user='<username here>',
                            passwd='<password here>',
