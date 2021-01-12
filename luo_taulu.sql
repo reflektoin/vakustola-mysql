984c20b25a04f88a8978d99bae73ff8fd0b7445b
Reflektoin <reflektoin@reflekto.in>
2021-01-12 20:16:45 +0200

    Poistetaan turhia tiedostoja

--- luo_taulu.sql
+++
@@ -1,2 +0,0 @@
-use <kannan_nimi>;
-create table test_hankinta (lasku_id varchar(12), tili varchar(100) , tiliointisumma numeric(18,2), tositepvm varchar(10), toimittaja_y_tunnus varchar(40), toimittaja_nimi  varchar(100), hankintayksikko_tunnus varchar(8), hankintayksikko varchar(128), ylaorganisaatio_tunnus varchar(8), ylaorganisaatio varchar(50), sektori varchar(10), tuote_palveluryhma varchar(90), hankintakategoria varchar(50));
